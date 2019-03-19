af9c62e043b6bcd9c076b51d0999996c4f8d76fe
Megan Bedell <megbedell@gmail.com>
2019-03-19 16:40:21 -0400

    consistent interpolation and outlier rejection of LCs

--- data/process_sc.py
+++ data/process_sc.py
@@ -10,7 +10,7 @@ data_dir = '/mnt/ceph/users/mbedell/tess/'
 def save_all_sc(filenames, out_file):
     N = len(filenames)
     with fits.open(filenames[0]) as hdul:
-        time = np.copy(hdul[1].data['TIME']) # assumes all TPFs have the same cadences
+        time = np.copy(hdul[1].data['TIME']) # assumes all TPFs have the same number of cadences
     M = len(time)
     
     # read all the files:
@@ -20,8 +20,12 @@ def save_all_sc(filenames, out_file):
     cameras = np.empty(N)
     bkgs = np.empty((N,M))
     bkg_errs = np.empty((N,M))
+    raw_bkgs = np.empty((N,M))
+    raw_bkg_errs = np.empty((N,M))
+    raw_times = np.empty((N,M))
+    raw_qualities = np.empty((N,M))
     bad = [] # store indices that may need deleting
-    tenpercent = int(N/10)
+    tenpercent = 1000 # guess
     for i,fn in enumerate(filenames):
         if i//tenpercent == 0:
             print('{0:3.1f}% done'.format(i/N * 100))
@@ -31,8 +35,18 @@ def save_all_sc(filenames, out_file):
                 rows[i] = np.copy(hdul[1].header['2CRV5P'])
                 ccds[i] = np.copy(hdul[0].header['CCD'])
                 cameras[i] = np.copy(hdul[0].header['CAMERA'])
-                bkgs[i,:] = np.copy(hdul[1].data['FLUX_BKG'][:,0,0]) # assumes all pixels get the same bkg
-                bkg_errs[i,:] = np.copy(hdul[1].data['FLUX_BKG_ERR'][:,0,0])
+                # get raw time series:
+                bkg = np.copy(hdul[1].data['FLUX_BKG'][:,0,0]) # assumes all pixels get the same bkg
+                bkg_err = np.copy(hdul[1].data['FLUX_BKG_ERR'][:,0,0])                
+                t = np.copy(hdul[1].data['TIME'])
+                raw_bkgs[i,:] = np.copy(bkg)
+                raw_bkg_errs[i,:] = np.copy(bkg_err)
+                raw_times[i,:] = np.copy(t)
+                raw_qualities[i,:] = np.copy(hdul[1].data['QUALITY'])
+                # interpolate good quality measurements to standard time grid and save:
+                q = hdul[1].data['QUALITY'] == 0
+                bkgs[i,:] = np.interp(time, t[q], bkg[q])
+                bkg_errs[i,:] = np.interp(time, t[q], bkg_err[q])
         except:
             print('WARNING: disregarding file {0}'.format(fn))
             bad.append(i)
@@ -43,6 +57,10 @@ def save_all_sc(filenames, out_file):
         cameras = np.delete(cameras, bad)
         bkgs = np.delete(bkgs, bad, axis=0)
         bkg_errs = np.delete(bkg_errs, bad, axis=0)
+        raw_times = np.delete(raw_times, bad, axis=0)
+        raw_bkgs = np.delete(raw_bkgs, bad, axis=0)
+        raw_bkg_errs = np.delete(raw_bkg_errs, bad, axis=0)
+        raw_qualities = np.delete(raw_qualities, bad, axis=0)
         
     # save the data:
     bkg = np.nanmedian(bkgs, axis=0)
@@ -55,6 +73,10 @@ def save_all_sc(filenames, out_file):
         f.create_dataset('bkgs', data=bkgs)
         f.create_dataset('bkg_errs', data=bkg_errs)
         f.create_dataset('time', data=time)
+        f.create_dataset('raw_times', data=raw_times)
+        f.create_dataset('raw_bkgs', data=raw_bkgs)
+        f.create_dataset('raw_bkg_errs', data=raw_bkg_errs)
+        f.create_dataset('raw_qualities', data=raw_qualities)
     return
    
 
