647376ffdf469771aceaec9d7bf072ed5bf44a88
Rodrigo Luger <rodluger@gmail.com>
2019-03-24 09:54:23 -0400

    added all figs

--- tex/gen_links.py
+++ tex/gen_links.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
 hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode("utf-8")[:-1]
-slug = os.getenv("TRAVIS_REPO_SLUG", "user/repo")
+slug = os.getenv("TRAVIS_REPO_SLUG", "rodluger/earthshine")
 with open("gitlinks.tex", "w") as f:
     print(r"\newcommand{\codelink}[1]{\href{https://github.com/%s/blob/%s/notebooks/#1.ipynb}{\codeicon}\,\,}" % (slug, hash), file=f)
